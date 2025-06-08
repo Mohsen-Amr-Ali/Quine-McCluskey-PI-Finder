0eac52d6e9787ee42b58fd4136c3f4e36785ef30
MohsenAli-05 <en.mohsenali@gmail.com>
2025-06-08 06:15:12 +0300

    Finished functions. remaining: CLI and testing

--- Python/PI-FInder.py
+++ Python/PI-FInder.py
@@ -1,19 +1,13 @@
 # ========================================== CLASSES ========================================== #
 class minterm:
     def __init__(self, id, numOfVariables):
-        self.id = id
+        self.id = [id]
         self.binary = bin(self.id)[2:].zfill(numOfVariables)
         self.group = self.binary.count('1')
+        self.flag = False
 
-class CombinedTerm:
-    def __init__(self, binary, original_ids):
-        self.binary = binary              
-        self.original_ids = original_ids  
-        self.group = self.binary.count('1')  
-        self.combined = False             
-
-    def __repr__(self):
-        return f"CombinedTerm(binary='{self.binary}', ids={self.original_ids}, combined={self.combined})"
+    def __str__(self):
+        return f"{','.join(str(i) for i in self.id)}: {self.binary}"
 
 
 # ========================================== FUNCTIONS ========================================== #
@@ -22,6 +16,99 @@ def vars_generator(numOfVariables):
     for i in range(numOfVariables):
         vars.append(chr(ord('a') + i)) #chr betconvert el unicode integer into a character
     return vars
+
+def combine_terms(mintermsList):
+    groups = {} #key -> grp num, value -> list of terms in that group
+
+    for term in mintermsList:
+        group = term.group
+        if group not in groups:
+            groups[group] = []
+        groups[group].append(term)
+    
+    combinedTerms = [] 
+    groupNumbers = sorted(groups.keys())
+
+    for i in range(len(groupNumbers) - 1):
+        groupA = groups[groupNumbers[i]]
+        groupB = groups[groupNumbers[i + 1]]
+
+        for term1 in groupA:
+            for term2 in groupB:
+                diffCount = 0
+                newBinary = ""
+                for b1, b2 in zip(term1.binary, term2.binary):
+                    if b1 != b2:
+                        if b1 == '-' or b2 == '-':
+                            diffCount = 2 
+                            break
+                        diffCount += 1
+                        newBinary += '-'
+                    else:
+                        newBinary += b1
+
+                if diffCount == 1:
+                    new_ids = []
+                    for id in term1.id:
+                        if id not in new_ids:
+                            new_ids.append(id)
+                    for id in term2.id:
+                        if id not in new_ids:
+                            new_ids.append(id)
+
+                    new_term = minterm(0, len(newBinary)) 
+                    new_term.id = sorted(new_ids)
+                    new_term.binary = newBinary
+                    new_term.group = newBinary.count('1')
+                    new_term.flag = False
+
+                    combinedTerms.append(new_term)
+                    term1.flag = True
+                    term2.flag = True
+
+    return combinedTerms
+
+def find_PIs_and_EPIs(mintermList):
+    allPIs = []
+    currentTerms = mintermList
+
+    while True:
+        combined = combine_terms(currentTerms)
+
+        for term in currentTerms:
+            if not term.flag:
+                already_included = False
+                for existing in allPIs:
+                    if term.id == existing.id and term.binary == existing.binary:
+                        already_included = True
+                        break
+                if not already_included:
+                    allPIs.append(term)
+
+        if not combined:
+            break
+
+        currentTerms = combined
+
+    minterm_to_implicants = {} #key -> minterm id from original mintermsList, values -> id of prime implicants that contain that minterm
+    for minterm in mintermList:
+        for pi in allPIs:
+            if minterm.id[0] in pi.id:
+                if minterm.id[0] not in minterm_to_implicants:
+                    minterm_to_implicants[minterm.id[0]] = []
+                minterm_to_implicants[minterm.id[0]].append(pi)
+
+    epis = []
+    for minterm_id, implicants in minterm_to_implicants.items():
+        if len(implicants) == 1:
+            epi = implicants[0]
+            if epi not in epis:
+                epis.append(epi)
+
+    return allPIs, epis
+
+
+        
 
 # ========================================== MAIN FUNCTION ========================================== #
 
